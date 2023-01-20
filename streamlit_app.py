bdd86e38565cfe51fa11c8742298e94692f88bf0
cva-inetum <119584836+cva-inetum@users.noreply.github.com>
2023-01-20 15:01:54 +0100

    -
    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,5 @@
-import pandas as pd
-import streamlit as st
+# streamlit_app.py
 
-st.title("Hi Anber! Welcome to Streamlit!")
+from platform import python_version
+st.write(python_version())
 
-st.write("My first DataFrame")
-
-st.write(
-pd.DataFrame({
-    'A': [1, 5, 9, 7],
-    'B': [3, 2, 4, 8]
-  })
-)
